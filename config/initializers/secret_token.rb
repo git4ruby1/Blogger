4fef91899919270a2975ee242d63a2a9c9df83a2
Mohit Ch <mohit@Mohits-MacBook-Pro.local>
2014-10-14 20:36:26 -0700

    trial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '076c96c5f76706c75ed317cb3c2fb71edb60e0009655b8d6787b30717e5fde8748f46632e6c213570a8c44aaa5c82a6be9a740950d22440885346f9646df4d03'
